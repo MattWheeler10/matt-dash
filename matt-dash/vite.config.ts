f01b497cac7f1a8e99d20f54280883068affa0c8
Matthew Wheeler <matthewwheeler66@gmail.com>
2026-03-16 10:37:10 +0000

    load local env for football api

--- matt-dash/vite.config.ts
+++ matt-dash/vite.config.ts
@@ -1,28 +1,34 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: '/',
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
-  },
-  server: {
-    proxy: {
-      '/api/football': {
-        target: 'https://api.football-data.org',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api\/football/, ''),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    base: '/',
+    plugins: [
+      vue(),
+      vueDevTools(),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
       },
+    },
+    server: {
+      proxy: {
+        '/api/football': {
+          target: 'https://api.football-data.org',
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api\/football/, ''),
+          headers: {
+            'X-Auth-Token': env.VITE_FOOTBALL_API_KEY ?? '',
+          },
+        },
       '/api/f1': {
         target: 'https://api.jolpi.ca',
         changeOrigin: true,
@@ -61,4 +67,5 @@ export default defineConfig({
       },
     },
   },
+  }
 })
